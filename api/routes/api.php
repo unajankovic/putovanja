3fcb990a04fccd55401b3d58d9d1bc7c9f3c5ad5
Una Jankovic <unajankovic39215@gmail.com>
2025-02-11 19:57:39 +0100

    rute za kontrolere

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,3 +6,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::resource('/drzave', \App\Http\Controllers\DrzavaController::class);
+Route::post('/prijava', [\App\Http\Controllers\PrijavaController::class, 'prijava']);
+Route::post('/registracija', [\App\Http\Controllers\PrijavaController::class, 'registracija']);
+
+//gmora biti autentifikovano
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/odjava', [\App\Http\Controllers\PrijavaController::class, 'odjava']);
+    Route::get('/putovanja', [\App\Http\Controllers\PutovanjeController::class, 'index']);
+    Route::get('/putovanja/{id}', [\App\Http\Controllers\PutovanjeController::class, 'show']);
+    Route::post('/putovanja', [\App\Http\Controllers\PutovanjeController::class, 'store']);
+    Route::put('/putovanja/{id}', [\App\Http\Controllers\PutovanjeController::class, 'update']);
+    Route::delete('/putovanja/{id}', [\App\Http\Controllers\PutovanjeController::class, 'destroy']);
+
+    Route::get('/korisnici/{id}/putovanja', [\App\Http\Controllers\PutovanjeController::class, 'pronadjiPoKorisniku']);
+    Route::get('/drzave/{id}/putovanja', [\App\Http\Controllers\PutovanjeController::class, 'pronadjiPoDrzavi']);
+
+    //komentari
+    Route::get('/komentari', [\App\Http\Controllers\KomentarController::class, 'index']);
+    Route::get('/putovanja/{putovanje_id}/komentari', [\App\Http\Controllers\KomentarController::class, 'pronadjiPoPutovanju']);
+    Route::post('/komentari', [\App\Http\Controllers\KomentarController::class, 'store']);
+
+
+    //slike
+    Route::get('/putovanja/{putovanje_id}/slike', [\App\Http\Controllers\SlikaController::class, 'pronadjiPoPutovanju']);
+    Route::post('/slike', [\App\Http\Controllers\SlikaController::class, 'store']);
+});
